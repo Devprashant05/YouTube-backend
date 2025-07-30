97a27f67c49d0151051b75c0c95e0ae3f8f4cf21
Devprashant05 <prashantsehgal03@gmail.com>
2025-07-30 17:44:29 +0530

    Add routes for user controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,13 @@ import {
     loginUser,
     logoutUser,
     refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetail,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,5 +38,19 @@ userRouter.route("/login").post(loginUser);
 // secured routes
 userRouter.route("/logout").post(verifyJWT, logoutUser);
 userRouter.route("/refresh-token").post(refreshAccessToken);
+
+// Edit routes
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword);
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetail);
+userRouter
+    .route("/update-avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+userRouter
+    .route("/update-cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+userRouter.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+userRouter.route("/history").get(verifyJWT, getUserWatchHistory);
 
 export default userRouter;
